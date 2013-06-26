98b5eda890068495733073e3e44e36ee4bb33554
MBing <bing.martin.photography@gmail.com>
2013-06-26 15:12:57 +0200

    aanpassingen voor datum en bijhorende info toevoegen voor datum functies

--- DB_SQL/Oefening12-oplossingen.sql
+++ DB_SQL/Oefening12-oplossingen.sql
@@ -12,30 +12,57 @@ VALUES ("artikelke1","artikel de beschrijving van het eerste",4.56,CURDATE(),CUR
 ("artikelke3","artikel de beschrijving van het derde",14.86,CURDATE(),CURDATE())
 
 # Pas 1 record aan zodat de date_creation van dat artikel niet langer gelijk is aan vandaag, maar 14 dagen geleden
-UPDATE `articles` SET `date_creation`=CURDATE()-14 WHERE name="artikelke1"
+UPDATE `articles` 
+SET `date_creation`=date_creation-14 # dit is geen veilige manier, bv als date_creation == 10.. -4 als resultaat!!
+WHERE name="artikelke1"
 # Toon nu enkel die records waarbij het artikel moet aangemaakt zijn in de laatste 7 dagen
-SELECT * FROM articles WHERE date_creation > (CURDATE()-7)
+SELECT * FROM articles 
+WHERE date_creation >= CURDATE()-7 # ook zelfde probleem hier, niet de correcte manier van werken
 # Toon de Afrikaanse landen waar de levensverwachting hoger is dan 60 in hoofdletters en de 
 	# Afrikaanse landen waar de levensverwachting lager of gelijk is dan 60 in kleine letters 
 	# sorteer op naam land alfabetisch
-SELECT UCASE(name) AS country FROM country WHERE continent="Africa" AND lifeexpectancy > 60
+SELECT UCASE(name) AS country 
+FROM country 
+WHERE continent="Africa" AND lifeexpectancy > 60
 UNION
-SELECT LCASE(name) FROM country WHERE continent="Africa" AND lifeexpectancy <= 60
-ORDER BY country #je kan geen order doen op iets dat niet weergegeven wordt
-
+SELECT LCASE(name) 
+FROM country 
+WHERE continent="Africa" AND lifeexpectancy <= 60
+ORDER BY country # je kan geen order doen op iets dat niet weergegeven wordt, daarom dus een alias aanmaken
+				 # of 1 gebruiken kan ook
 # Geef de citycode (=eerste 3 karakters) van elke stad waarvan het land geregeerd wordt door Elisabeth II
-SELECT MID(city.name,1,3) AS stadscode FROM city INNER JOIN country ON city.countrycode=country.code
+SELECT LEFT(city.name,3) AS stadscode 
+FROM city 
+INNER JOIN country ON city.countrycode=country.code
 WHERE headofstate="Elisabeth II"
 # Geef de namen van de steden waarvan de benaming langer is dan 20 karakters
-SELECT name FROM city GROUP BY name HAVING LENGTH(name) > 20
+SELECT name 
+FROM city 
+GROUP BY name 
+HAVING LENGTH(name) > 20 # WHERE zal wel werken, LENGTH is geen aggregate functie
+						 # maar toch best consistent zijn in gebruik
+						 # best dus altijd HAVING gebruiken!
 # Toon de naam van de landen waar Elisabeth II regeert. 
 	# Toon ook in elke record Her Majesty The Queen Elisabeth II
-SELECT name, CONCAT('Her Majesty The Queen ',headofstate) FROM country
+SELECT name, CONCAT('Her Majesty The Queen ',headofstate) 
+FROM country
 WHERE headofstate="Elisabeth II"
 # Toon de gemiddelde levensverwachting in de wereld. Rond af tot 2 cijfers na de komma
-SELECT ROUND(AVG(lifeexpectancy), 2) AS life_world_exp FROM country
+SELECT ROUND(AVG(lifeexpectancy), 2) AS life_exp_world FROM country
 # Toon een willekeurig land en de huidige systeemdatum&tijd in formaat DD-MM-YYYY HH:MM:SS
-SELECT name, DATE_FORMAT(NOW(),'%d-%m-%Y %T') AS cur_date FROM country
+SELECT name, DATE_FORMAT(NOW(),'%d-%m-%Y %T') AS cur_date 
+FROM country
 ORDER BY RAND() LIMIT 1
 # Toon een willekeurig artikel uit uw webshop (formaat datum creatie en laatst gewijzigd = dd/mm/’yy
-SELECT name, DATE_FORMAT(date_last_modified, '%d/%m/\'%y') FROM articles ORDER BY RAND() LIMIT 1
+SELECT name, DATE_FORMAT(date_creation, '%d/%m/\'%y') AS was_made, 
+DATE_FORMAT(date_last_modified, '%d/%m/\'%y') AS was_last_mod 
+FROM articles 
+ORDER BY RAND() LIMIT 1
+
+# Datums vergelijken EXTRA:
+DATE_ADD()
+DATE_SUB()
+DATEDIFF(datum1, datum2)
+vb.
+	DATEDIFF(CURDATE(), date_creation) <= 7
+	DATEDIFF()
