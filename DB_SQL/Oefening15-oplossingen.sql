b596a16b772f0da8b163148b9374b1cbdecb1596
MBing <bing.martin.photography@gmail.com>
2013-06-28 11:38:51 +0200

    adjustments to ex 15, extra comments to clarify

--- DB_SQL/Oefening15-oplossingen.sql
+++ DB_SQL/Oefening15-oplossingen.sql
@@ -14,8 +14,8 @@ ALTER TABLE cars DROP promoprice
 ALTER TABLE cars CHANGE description description TEXT
 # Breidt de volgende velden uit met de opgegeven veldeigenschappen:
 	# id : attribuut unsigned
-ALTER TABLE cars CHANGE id id INT(10) UNSIGNED
+ALTER TABLE cars CHANGE id id INT UNSIGNED
 	# type : commentaar: Het type van de wagen
 ALTER TABLE cars CHANGE type type VARCHAR(50) COMMENT "Het type van de wagen"
-	# description: karakterset utf8 en collatie utf8_general_ci
+	# description: karakterset utf8 en collatie utf8_general_ci #case insensitive
 ALTER TABLE cars CHANGE description description TEXT CHARACTER SET utf8 COLLATE utf8_general_ci
